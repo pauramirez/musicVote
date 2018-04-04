3e4b051bd4d1931ca471a46ef6e9479de248621d
CamiloC-Wk <32606059+CamiloC-Wk@users.noreply.github.com>
2018-04-04 11:19:48 -0500

    Update posts.js

--- imports/api/posts.js
+++ imports/api/posts.js
@@ -1,4 +1,4 @@
 import { Mongo } from "meteor/mongo";
 
-
+//Camilo A Carrillo N: Para mejorar la seguridad pueden usar Methods y llamarlos mediante Meteor.Call("Nombre del metodo", parametros);
 export const Posts = new Mongo.Collection("posts");
